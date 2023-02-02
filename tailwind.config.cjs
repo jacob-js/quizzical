137e06559a89ca74becb5ff798fc622e08911067
Jacob Dev <mercihabam@gmail.com>
2023-02-02 16:03:40 +0200

    initializing the repo

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          800: '#4D5B9E',
+          900: '#293264'
+        }
+      },
+      fontFamily: {
+        karla: ['Karla', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
