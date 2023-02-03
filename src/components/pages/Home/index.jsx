b878bb6d783724cccef384eb9f1b7f0a68f857c7
Jacob Dev <mercihabam@gmail.com>
2023-02-03 14:56:30 +0200

    feat: change description text's line-height

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -3,9 +3,9 @@ import Button from '../../ui/Button'
 
 function Home() {
   return (
-    <div className='w-full h-full flex flex-col items-center justify-center'>
-        <div className="text-7xl text-primary-900 font-bold -tracking-wide">Quizzical</div>
-        <div className="text-primary-900 text-xl text-center mt-5 lg:max-w-lg mb-7 -tracking-wide">Exercise your knowledge to keep your brain aware of all chunks of science you have</div>
+    <div className='h-full flex flex-col items-center justify-center'>
+        <div className="text-4xl sm:text-7xl text-primary-900 font-bold -tracking-wide">Quizzical</div>
+        <div className="text-primary-900 text-xl text-center mt-5 lg:max-w-lg mb-7 -tracking-wide leading-6">Exercise your knowledge to keep your brain aware of all chunks of science you have</div>
         <Button>Start Quiz</Button>
     </div>
   )
