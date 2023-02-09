07a9ad3340a954c42fc564f99f2c34fdff7dabcf
Jacob Dev <mercihabam@gmail.com>
2023-02-09 17:01:25 +0200

    chore: integrate react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,27 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/pages/Home'
 import theme from './config/chakraTheme'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  }
+  // {
+  //   path: '/quiz'
+  // }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <Layout>
-        <Home />
+        <RouterProvider router={router} />
       </Layout>
     </ChakraProvider>
   </React.StrictMode>,
