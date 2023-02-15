335703b839f691df89ab2c5df949f60e475ecc65
Jacob Dev <mercihabam@gmail.com>
2023-02-15 16:01:42 +0200

    feat: fetched questions from the api

--- src/components/pages/Quiz/QuizItem/index.jsx
+++ src/components/pages/Quiz/QuizItem/index.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 import OptionTag from '../../../ui/OptionTag'
 
-function QuizItem({question, incorrect_answers, correct_answer}) {
+function QuizItem({item, onChoose}) {
+    const {question, incorrect_answers, correct_answer, succeed} = item;
     const [selected, setSelected] = useState('');
+
   return (
     <div className='py-4 border-b border-light-blue'>
         <div className="text-primary-900 font-bold lg:text-xl">{question}</div>
         <div className="flex mt-5 flex-wrap gap-3">
             {
                 [...incorrect_answers, correct_answer].map((answer, index) =>(
-                    <OptionTag key={index} onClick={() =>setSelected(answer)} selected={answer === selected}>{answer}</OptionTag>
+                    <OptionTag key={index} onClick={() =>{setSelected(answer); onChoose(item, answer)}} selected={answer === selected} isValid={succeed} isInvalid={succeed === false} submitted={false}>{answer}</OptionTag>
                 ))
             }
         </div>
--- src/components/pages/Quiz/index.jsx
+++ src/components/pages/Quiz/index.jsx
@@ -1,72 +1,32 @@
-import React from 'react'
+import useAxios from 'axios-hooks';
+import React, { useEffect, useState } from 'react'
 import Button from '../../ui/Button'
 import QuizItem from './QuizItem'
 
 function Quiz() {
-    const items = [
-        {
-            "category": "Science: Computers",
-            "type": "boolean",
-            "difficulty": "medium",
-            "question": "The common software-programming acronym &quot;I18N&quot; comes from the term &quot;Interlocalization&quot;.",
-            "correct_answer": "False",
-            "incorrect_answers": [
-              "True"
-            ]
-          },
-          {
-            "category": "Science: Computers",
-            "type": "multiple",
-            "difficulty": "medium",
-            "question": "The computer OEM manufacturer Clevo, known for its Sager notebook line, is based in which country?",
-            "correct_answer": "Taiwan",
-            "incorrect_answers": [
-              "United States",
-              "Germany",
-              "China (People&#039;s Republic of)"
-            ]
-          },
-          {
-            "category": "Science: Computers",
-            "type": "multiple",
-            "difficulty": "easy",
-            "question": "In &quot;Hexadecimal&quot;, what color would be displayed from the color code? &quot;#00FF00&quot;?",
-            "correct_answer": "Green",
-            "incorrect_answers": [
-              "Red",
-              "Blue",
-              "Yellow"
-            ]
-          },
-          {
-            "category": "Science: Computers",
-            "type": "multiple",
-            "difficulty": "easy",
-            "question": "Which computer language would you associate Django framework with?",
-            "correct_answer": "Python",
-            "incorrect_answers": [
-              "C#",
-              "C++",
-              "Java"
-            ]
-          },
-          {
-            "category": "Science: Computers",
-            "type": "multiple",
-            "difficulty": "easy",
-            "question": "How long is an IPv6 address?",
-            "correct_answer": "128 bits",
-            "incorrect_answers": [
-              "32 bits",
-              "64 bits",
-              "128 bytes"
-            ]
-          }
-    ]
+    const [{loading, data}] = useAxios('https://opentdb.com/api.php?amount=5&category=18');
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() =>{
+      (() =>{
+        data?.results && setQuestions(data.results)
+      })()
+    }, [data]);
+
+    const onChoose = (question, choice) =>{
+      question.succeed = choice === question.correct_answer ? true: false;
+      setQuestions(state => {
+        const newQuestions = [];
+        state.forEach(quest => quest.question !== question.question ? newQuestions.push(quest): newQuestions.push(question));
+        return newQuestions;
+      });
+    };
+
   return (
     <div className='flex flex-col gap-5 pb-20'>
         {
-            items.map((item, index) => <QuizItem {...item} key={index} />)
+            loading ? "Loading..." :
+            questions.map((item, index) => <QuizItem item={item} key={index} onChoose={onChoose} />)
         }
         <Button>Check answers</Button>
     </div>
