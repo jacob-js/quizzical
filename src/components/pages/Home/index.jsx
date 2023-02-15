9e4c02136c8aa3f2c9bf69075849d8a15b2b8c20
Jacob Dev <mercihabam@gmail.com>
2023-02-15 18:07:35 +0200

    feat: change home page intro text

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -9,7 +9,7 @@ function Home() {
   return (
     <div className='h-full flex flex-col items-center justify-center'>
         <div className="text-4xl sm:text-7xl text-primary-900 font-bold -tracking-wide">Quizzical</div>
-        <div className="text-primary-900 text-xl text-center mt-5 lg:max-w-lg mb-7 -tracking-wide leading-6">Exercise your knowledge to keep your brain aware of all chunks of science you have</div>
+        <div className="text-primary-900 text-xl text-center mt-5 lg:max-w-lg mb-7 -tracking-wide leading-6">Exercise your knowledge to keep your brain active in computer science field</div>
         <Button onClick={() =>navigate('/quiz')}>Start Quiz</Button>
     </div>
   )
